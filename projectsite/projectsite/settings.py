740297c4dc2dbf27d94b17ffdf2b8a61bb8f6901
Bry <etoquillab@gmail.com>
2025-03-25 12:15:17 +0800

    setup database

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'projectsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'psusite',
+        'USER': 'root',
+        'PASSWORD': '202280287PSU',
+        'PORT': '3306',
+        'HOST': 'localhost'
     }
 }
 
